e95ad3d3dd2b03ab86e04f268961e2bce3ce9850
Tanmay Patil <tany.patil77@gmail.com>
2023-05-22 00:16:50 +0530

    added test to parse xml using schema

--- xmldemo/src/main/java/com/xmldemo/XmlUtils.java
+++ xmldemo/src/main/java/com/xmldemo/XmlUtils.java
@@ -1,6 +1,8 @@
 package com.xmldemo;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,13 +14,14 @@ import org.apache.xmlbeans.XmlOptions;
 public class XmlUtils {
 
     private String basePath = "";
+    SchemaTypeLoader m_contextXchemaTypeLoader = null;
 
     public void setBasePath(String basePath) {
         this.basePath = basePath;
     }
 
     public SchemaTypeLoader readSchema(String xsdFile) {
-        SchemaTypeLoader m_contextXchemaTypeLoader = null;
+
         try {
             String finalPath = this.basePath + "\\" + xsdFile;
             List<XmlObject> listSchemaFiles = new ArrayList<XmlObject>();
@@ -27,17 +30,45 @@ public class XmlUtils {
             if (schemaFile.exists()) {
                 listSchemaFiles.add(XmlObject.Factory.parse(schemaFile));
             }
-            m_contextXchemaTypeLoader
+            this.m_contextXchemaTypeLoader
                     = XmlBeans.compileXsd(listSchemaFiles.toArray(new XmlObject[]{}), XmlBeans.getContextTypeLoader(),
                             new XmlOptions().setLoadMessageDigest());
-            m_contextXchemaTypeLoader
+            this.m_contextXchemaTypeLoader
                     = XmlBeans.typeLoaderUnion(new SchemaTypeLoader[]{m_contextXchemaTypeLoader, XmlBeans.getContextTypeLoader()});
         } catch (Exception e) {
             System.out.println("exception in schema compilation " + xsdFile);
             System.out.println("exception  : " + e);
         }
         return m_contextXchemaTypeLoader;
+    }
 
+    public String readXml(String xmlFile) {
+        String finalStr = "";
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(this.basePath + "\\" + xmlFile));
+            System.out.println("opening " + xmlFile);
+            String curLine;
+
+            while ((curLine = bufferedReader.readLine()) != null) {
+                System.out.println(curLine);
+                finalStr += curLine;
+            }
+            System.out.println("final Xml " + finalStr);
+        } catch (Exception e) {
+            System.out.println("exception in reading xml" + e);
+        }
+        return finalStr;
+    }
+
+    public XmlObject parseXml(String name, String xml) {
+        XmlObject x = null;
+        try {
+            x = m_contextXchemaTypeLoader.parse(xml, null, null);
+        } catch (Exception e) {
+            System.out.println("parsing exception for : " + name);
+            System.out.println("parseXml " + e);
+        }
+        return x;
     }
 
 }
--- xmldemo/src/test/java/com/xmldemo/AppTest.java
+++ xmldemo/src/test/java/com/xmldemo/AppTest.java
@@ -1,126 +1,46 @@
 package com.xmldemo;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.namespace.QName;
-
 import org.apache.xmlbeans.SchemaTypeLoader;
-import org.apache.xmlbeans.XmlBeans;
-import org.apache.xmlbeans.XmlCursor;
 import org.apache.xmlbeans.XmlObject;
-import org.apache.xmlbeans.XmlOptions;
 
 /**
- * Unit test for simple App.
+ * Unit test for apache xml bean usages
+ * This will allow to understand , how apache xmlbeans can be used to parse xml 
+ * do xpaths , create a xml document etc.
  */
 class AppTest {
 
-    /**
-     * Rigorous Test.
-     */
-    @Test
-    void testApp() {
-        assertEquals(1, 1);
+    @Test 
+    void loadxsd1() {
+        XmlUtils x = new XmlUtils();
+        x.setBasePath("C:\\Users\\u725561\\xmlbeans-demo\\xmldemo\\src\\test\\java\\com\\xmldemo");
+        SchemaTypeLoader s = x.readSchema("shiporder.xsd");
+        Assertions.assertNotNull(s);
     }
 
-    @Test
-    void testLoadSchema() {
-        SchemaTypeLoader m_contextXchemaTypeLoader;
-        try {
-            List<XmlObject> listSchemaFiles = new ArrayList<XmlObject>();
-            File schemaFile = null;
-            schemaFile = new File("./shiporder.xsd");
-            if (schemaFile.exists()) {
-                listSchemaFiles.add(XmlObject.Factory.parse(schemaFile));
-            }
-            m_contextXchemaTypeLoader
-                    = XmlBeans.compileXsd(listSchemaFiles.toArray(new XmlObject[]{}), XmlBeans.getContextTypeLoader(),
-                            new XmlOptions().setLoadMessageDigest());
-            m_contextXchemaTypeLoader
-                    = XmlBeans.typeLoaderUnion(new SchemaTypeLoader[]{m_contextXchemaTypeLoader, XmlBeans.getContextTypeLoader()});
-            
-            // Check whether so namespace exists 
-            boolean isExists = m_contextXchemaTypeLoader.isNamespaceDefined("so");
-            assertEquals(true,isExists);
-            
-            //isExists = m_contextXchemaTypeLoader.isNamespaceDefined("sa");
-            //assertEquals(true,isExists);
-
-        } catch (Exception e) {
-            System.out.println("exception in shiporder xsd parsing"+e);
-        }
-
+    @Test 
+    void loadxsd2() {
+        XmlUtils x = new XmlUtils();
+        x.setBasePath("C:\\Users\\u725561\\xmlbeans-demo\\xmldemo\\src\\test\\java\\com\\xmldemo");
+        SchemaTypeLoader s = x.readSchema("note.xsd");
+        Assertions.assertNotNull(s);
     }
-@Test
-    void testApp2() {
-        SchemaTypeLoader m_contextXchemaTypeLoader;
-        BufferedReader bufferedReader = null;
-        try {
-            List<XmlObject> listSchemaFiles = new ArrayList<XmlObject>();
-            File schemaFile = null;
-            schemaFile = new File("./note.xsd");
-            if (schemaFile.exists()) {
-                listSchemaFiles.add(XmlObject.Factory.parse(schemaFile));
-            }
-            m_contextXchemaTypeLoader
-                    = XmlBeans.compileXsd(listSchemaFiles.toArray(new XmlObject[]{}), XmlBeans.getContextTypeLoader(),
-                            new XmlOptions().setLoadMessageDigest());
-            m_contextXchemaTypeLoader
-                    = XmlBeans.typeLoaderUnion(new SchemaTypeLoader[]{m_contextXchemaTypeLoader, XmlBeans.getContextTypeLoader()});
-           
-           
-            bufferedReader = new BufferedReader(new FileReader("C:\\Users\\u725561\\xmlbeans-demo\\xmldemo\\src\\test\\java\\com\\xmldemo\\note.xml"));
-            System.out.println("opening note.xml" );
-            String curLine;
-            String finalStr = "";
-            while ((curLine = bufferedReader.readLine()) != null){
-                System.out.println(curLine);
-                finalStr += curLine;
-            }
-            System.out.println("final Xml "+finalStr);
-            XmlObject x = m_contextXchemaTypeLoader.parse(finalStr, null, null);
-            boolean y = x.isImmutable();
-            System.out.println( "value of y"+y);
-            XmlCursor z = x.newCursor();
-           XmlObject o = z.getObject();
-           if ( o != null) {
-            System.out.println("XmlObject is not null");
-           }
+
     
-           XmlObject xa = o.selectAttribute(new QName("http://www.w3.org/TR/html4/",   "h"));
-           if ( xa == null) {
-            final XmlCursor xmlcur = o.newCursor();
-            xmlcur.toStartDoc();
-            xmlcur.toFirstChild();
-            xa = xmlcur.getObject();
-            xa = o.selectAttribute(new QName("http://www.w3.org/TR/html4/",   "h"));
-           }
-           String uri = xa.schemaType().getName().getNamespaceURI();
-           System.out.println("uri is "+uri);
-
-        } catch (Exception e) {
-            System.out.println("exception in shiporder xsd parsing"+e);
-        }
-        finally{
-            try {
-            bufferedReader.close();
-            }
-            catch(Exception e) {
-                System.out.println("closing error in bufferedreader "+e);
-
-            }
-        }
-
+    @Test 
+    @DisplayName("parse note.xml using note.xsd and get XmlObject")
+    void parseNotesXml() {
+        XmlUtils x = new XmlUtils();
+        x.setBasePath("C:\\Users\\u725561\\xmlbeans-demo\\xmldemo\\src\\test\\java\\com\\xmldemo");
+        SchemaTypeLoader s = x.readSchema("note.xsd");
+        Assertions.assertNotNull(s);
+        String noteXml = x.readXml("note.xml");
+        XmlObject xo = x.parseXml("note.xml",noteXml);
+        Assertions.assertNotNull(xo);
     }
+
 }
